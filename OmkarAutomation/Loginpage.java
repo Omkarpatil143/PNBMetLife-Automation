b895bcb192ed9a77c0c1dca60747410af550e7df
omkarpatil143 <omkarpatilmech1@gmail.com>
2026-04-26 22:51:13 +0530

    Logincode

---
+++ OmkarAutomation/Loginpage.java
@@ -0,0 +1,7 @@
+package OmkarAutomation;
+
+public class Loginpage {
+    public static void main(String[] args) {
+        System.out.println("this is my second code i want to push it in github");
+    }
+}
